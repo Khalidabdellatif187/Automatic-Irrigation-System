d95490a195e0dde4ac13a62ac06da03e52c9b7fd
khalid  Abd-ellatif Khalid Abd-ellatif <103316778+Khalidabdellatif187@users.noreply.github.com>
2023-01-01 00:58:03 +0200

    Add files via upload

---
+++ Automatic irrigation system.sql
@@ -0,0 +1,5 @@
+create database irrigation_system;
+Use  irrigation_system;
+
+/** please note that I use ddlauto in springboot properties.file so table will be created automatically
+/** if you do not need to use ddlauto I write instructions of how to create a table in READ.MD
