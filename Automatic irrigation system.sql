067517478c74edc057207d483a52f64cb9228d85
khalid  Abd-ellatif Khalid Abd-ellatif <103316778+Khalidabdellatif187@users.noreply.github.com>
2023-01-01 18:41:08 +0200

    Update Automatic irrigation system.sql

--- Automatic irrigation system.sql
+++ Automatic irrigation system.sql
@@ -3,3 +3,7 @@ Use  irrigation_system;
 
 /** please note that I use ddlauto in springboot properties.file so table will be created automatically
 /** if you do not need to use ddlauto I write instructions of how to create a table in READ.MD
+
+insert into details(water_amount , area , crop) values ('100 m^3' , '250 m^2' , 'watermelon');
+insert into details(water_amount , area , crop) values ('100 m^3' , '250 m^2' , 'orange');
+insert into details(water_amount , area , crop) values ('100 m^3' , '250 m^2' , 'pepper');
